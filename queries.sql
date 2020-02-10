9100cd62b8a47efbb0f4f4fc889067a8bd1b1dab
Logan Taylor <Stellarate.Official@gmail.com>
2020-02-09 18:14:21 -0600

    finsished project

--- queries.sql
+++ queries.sql
@@ -1,7 +1,15 @@
-## Part 1: Test it with SQL
+##Part 1
+EXPLAIN techjobs.job;
+DESCRIBE techjobs.job;
+SHOW FIELDS FROM techjobs.job;
+SHOW COLUMNS FROM techjobs.job;
+##Part2
+SELECT name, location
+FROM employer
+WHERE location
+IN (SELECT location FROM employer WHERE location="St. Louis City");
 
-## Part 2: Test it with SQL
+#Part3
 
-## Part 3: Test it with SQL
-
-## Part 4: Test it with SQL
+##Part4
+SELECT name, description FROM techjobs.skill WHERE id in(SELECT skills_id FROM job_skills WHERE skills_id IS NOT NULL);
